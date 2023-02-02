e573483ec396db51450ad1974e1eacf70c78d4d8
Walter José <walterjosecruz@gmail.com>
2023-02-02 14:27:00 -0300

    Adicionado json

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+app.get("/", (req, res) => {
+    return res.json({
+        titulo: "Teste de API REST"
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080...");
+});
