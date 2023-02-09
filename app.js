e31475198083aada5b41d48cc1443e130bad986b
Walter José <walterjosecruz@gmail.com>
2023-02-09 17:11:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
+
+require("./models/Artigo")
 const mongoose = require('mongoose');
+const Artigo = mongoose.model('artigo')
 
 app.use(express.json())
 app.get("/", (req, res) => {
-    return res.json({
-        titulo: "Teste de API REST"
-    });
+    Artigo.find({}).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        })
+    })
 });
+
+app.post("/artigo", (req, res) => {
+    const artigo = Artigo.create(req.body, (err) => {
+        if(err) return res.statusCode(400).json({
+            error: true,
+            message: "Erro: Artigo não cadastrado!"
+        })
+        return res.status(200).json({
+            error: false,
+            message: "Artigo cadastrado com sucesso!"
+        })
+    })
+})
 
 mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://127.0.0.1:27017/projetinhofellas').then(() => {
