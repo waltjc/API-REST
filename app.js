cfe73be877acb921dfef8881966e65516bd8c5ff
Walter José <walterjosecruz@gmail.com>
2023-02-09 14:30:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.json())
 app.get("/", (req, res) => {
@@ -7,6 +8,13 @@ app.get("/", (req, res) => {
         titulo: "Teste de API REST"
     });
 });
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://localhost/projetinhofellas').then(() => {
+    console.log('Conexão estabelecida!')
+}).catch(() => {
+    console.log('Conexão não estabelecida!');
+})
 
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080...");
