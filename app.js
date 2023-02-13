75d4aaee069a9b4f351653e4b8ce7d4fa8afd42f
Walter José <walterjosecruz@gmail.com>
2023-02-13 16:46:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,19 +3,40 @@ const app = express();
 
 require("./models/Artigo")
 const mongoose = require('mongoose');
+const cors = require('cors');
 const Artigo = mongoose.model('artigo')
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+    //console.log("Acessou o middleware");
+    //res.header("Access-Control-Allow-Origin", "http://localhost:8080")
+    res.header("Access-Control-Allow-Origin", 'PUT, POST, DELETE')
+    app.use(cors());
+    next();
+})
+
 app.get("/", (req, res) => {
     Artigo.find({}).then((artigo) => {
         return res.json(artigo);
-    }).catch((erro) => {
+    }).catch(() => {
         return res.status(400).json({
             error: true,
             message: "Nenhum artigo encontrado!"
         })
     })
 });
+
+app.get("/artigo/:id", (req, res) => {
+    Artigo.findOne({_id:req.params.id}).then((artigo) => {
+        return res.json(artigo);
+    }).catch((error) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado para esse ID"
+        })
+    })
+})
 
 app.post("/artigo", (req, res) => {
     const artigo = Artigo.create(req.body, (err) => {
@@ -26,6 +47,32 @@ app.post("/artigo", (req, res) => {
         return res.status(200).json({
             error: false,
             message: "Artigo cadastrado com sucesso!"
+        })
+    })
+})
+
+app.put("/artigo/:id", (req, res) => {
+    const artigo = Artigo.updateOne({_id: req.params.id}, req.body, (error) => {
+        if(error) return res.status(400).json({
+            error: true,
+            message: "Erro: não foi possível editar o artigo"
+        })
+        return res.json({
+            error: false,
+            message: "Artigo editado com sucesso!"
+        })
+    })
+})
+
+app.delete("/artigo/:id", (req, res) => {
+    const artigo = Artigo.deleteOne({_id: req.params.id}, req.body, (error) => {
+        if(error) return res.status(400).json({
+            error: true,
+            message: "Erro: não foi possível editar o artigo"
+        })
+        return res.json({
+            error: false,
+            message: "Artigo editado com sucesso!"
         })
     })
 })
